3eb062c3003d92ef4513f1cb84614c229aa0b13f
iliescumichele <iliescumichele@gmail.com>
2022-04-26 23:39:47 +0200

    Function that asks user for numbers ; function that checks the if the user wrote the write numbers (bug to fix line 16)

--- src/script.js
+++ src/script.js
@@ -7,7 +7,23 @@ function initGame(){
     const arrNumRandom = [];
     getRndNum(arrNumRandom);
 
-    showNumbersTimed(boxHtmlNum, arrNumRandom, 5)
+    showNumbersTimed(boxHtmlNum, arrNumRandom, 5);
+
+    const arrNumUser = [];
+    setTimeout(function(){ askUser(arrNumUser) }, 5.5*1000);
+
+    let checkArrayLenght = false;
+    while(checkArrayLenght == false){
+        if(checkArrayLenght == true){
+            let definitiveCheck = checkAnswers(arrNumRandom, arrNumRandom);
+        };
+    };
+
+    if(definiteveCheck == true){
+        boxHtmlNum.innerHTML = "Good job";
+    } else {
+        boxHtmlNum.innerHTML = "Try again u lost";
+    };
 
 };
 
@@ -16,7 +32,11 @@ function initGame(){
 
 
 
-
+/**
+ * Inserisce nell'array dei numeri random
+ * @param {array} array 
+ * @returns l'array inserito con i numeri random
+ */
 function getRndNum(array) {
     for(let i = 0; i < 5; i++){
         array[i] = Math.floor(Math.random() * 201 );
@@ -24,7 +44,7 @@ function getRndNum(array) {
     }
 
     return array;
-}
+};
 
 
 /**
@@ -35,7 +55,37 @@ function getRndNum(array) {
  */
 function showNumbersTimed(wherHtml, array, sec){
     wherHtml.innerHTML = `<strong>${array[0]} - ${array[1]} - ${array[2]} - ${array[3]} - ${array[4]}</strong>`;
+
     setTimeout(function(){
-        wherHtml.classList.add("hide");
+        boxHtmlNum.innerHTML = "";
     }, sec*1000);
+};
+
+
+/**
+ * Chiede all'utente i numeri e li salva in un array
+ * @param {array} array 
+ * @returns l'array con i numeri inseriti dall'utente
+ */
+function askUser(array){
+    for(let i=0 ; i<5 ; i++){
+        array[i] = parseInt(prompt("Scrivi UNO dei numeri che sono apparsi"));
+    };
+
+    checkArrayLenght = true;
+
+    return array;
+};
+
+
+function checkAnswers(arrayPc, arrayUser){
+    for(let i=0 ; i < arrayPc.lenght ; i++){
+        for(let j=0 ; j < arrayPc.lenght ; j++){    
+            if(! (arrayPc[i] === arrayUser[j])){
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
